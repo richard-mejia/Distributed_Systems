d97f14f60b4517648f8b48d341a2b5e5e29f5974
MarioG <enigsea@gmail.com>
2016-08-23 22:01:35 -0500

    the files publications y methods are added

--- slack/server/seeder.js
+++ slack/server/seeder.js
@@ -1,17 +1,19 @@
 Meteor.startup(function() {
 
+  Meteor.users.remove({});
+  Accounts.createUser({
+    username: "mario",
+    email: "mario@gmail.com",
+    password: "123456"
+  });
+
   Factory.define('message', Messages, {
     text: function() {
     	return Fake.sentence();
-    }
+    },
+    user: Meteor.users.findOne()._id,
+    timestamp: Date.now()
   });
 
-  // Add this if you want to remove all messages before seeding
-  Messages.remove({});
-
-  if (Messages.find({}).count() === 0) {
-    _(10).times(function(n) {
-      Factory.create('message');
-    });
-  }
+  //Messages.remove({});
 });
